de1f6edb2a53b562215edb49ec3123596cfc9b10
Brenden <furp7691@yahoo.com>
2020-03-12 21:06:07 -0400

    Finally finished creating league

---
+++ SpectatorFootball/League/New_League_Structure.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SpectatorFootball.Models;
+
+//This class is used to hold the structure of the new league before it is saved to the database.
+//This needs to be done since the league database file is not even created at this point.
+namespace SpectatorFootball.League
+{
+    public class New_League_Structure
+    {
+        public SpectatorFootball.Models.League League { get; set; }
+        public DBVersion DBVersion { get; set; }
+
+    }
+}
